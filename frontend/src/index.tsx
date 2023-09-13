6af8521b741eab2fd0164b2a727550bfbb31ad4e
jbundziow <jakub.bundziow@gmail.com>
2023-09-13 19:13:02 +0200

    added react-router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Blogs from "./pages/Blogs";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="blogs" element={<Blogs />} />
+      </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
