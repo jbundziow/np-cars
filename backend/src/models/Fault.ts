8a5c1106e82d8f4a7afdb654dc9683a522393de9
jbundziow <jakub.bundziow@gmail.com>
2023-10-25 08:23:53 +0200

    added 2 models

---
+++ backend/src/models/Fault.ts
@@ -0,0 +1,121 @@
+const {DataTypes} = require('sequelize');
+
+
+
+import sequelize from "../database/database";
+const FaultModel = sequelize.define('Fault', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true
+      },
+      carID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      // carFullname: {
+      //   type: DataTypes.STRING,
+      //   allowNull: false,
+      // },
+      // carImg: {
+      //   type: DataTypes.STRING,
+      //   allowNull: false,
+      // },
+      userID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      // userFullname: {
+      //   type: DataTypes.STRING,
+      //   allowNull: false,
+      // },
+      moderatorID: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      // moderatorFullname: {
+      //   type: DataTypes.STRING,
+      //   allowNull: true,
+      // },
+      lastChangeAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.ENUM('pending', 'accepted', 'finished', 'cancelled'),
+        allowNull: false,
+      },
+      resultDescription: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      repairCost: {
+        type: DataTypes.number,
+        allowNull: true,
+      },
+
+})
+
+
+
+class Fault {
+    constructor(
+        private id: number | null,
+        private carID: number,
+        // private carFullname: string,
+        // private carImg: string,
+        private userID: number,
+        // private userFullname: string,
+        private moderatorID: number | null,
+        // private moderatorFullname: string,
+        //private createdAt: string,
+        private lastChangeAt: string | null,
+        private title: string,
+        private description: string,  
+        private status: 'pending' | 'accepted' | 'finished' | 'cancelled',
+        private resultDesctiption: string | null,
+        private repairCost: number | null,
+        ) {}
+
+    async addOneFault() {
+        await FaultModel.create({
+          id: this.id,
+          carID: this.carID,
+          // carFullname: this.carFullname,
+          // carImg: this.carImg,
+          userID: this.userID,
+          // userFullname: this.userFullname,
+          moderatorID: this.moderatorID,
+          // moderatorFullname: this.moderatorFullname,
+          //createdAt: this.createdAt,
+          lastChangeAt: this.lastChangeAt,
+          title: this.title,
+          description: this.description,
+          status: this.status,
+          resultDesctiption: this.resultDesctiption,
+          repairCost: this.repairCost,
+        })
+    }
+
+    static async fetchAll() {
+        return await FaultModel.findAll()
+    }
+
+    static async fetchOne(id: number) {
+        return await FaultModel.findOne({ where: { id: id } })
+    }
+}
+
+
+
+export default Fault;
---
+++ backend/src/models/Refueling.ts
@@ -0,0 +1,78 @@
+const {DataTypes} = require('sequelize');
+
+
+
+import sequelize from "../database/database";
+const RefuelingModel = sequelize.define('Refueling', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true
+      },
+      carID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      userID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      carMileage: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      numberOfLiters: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      costBrutto: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      isFuelCardUsed: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+
+
+})
+
+
+
+class Refueling {
+    constructor(
+        private id: number | null,
+        private carID: number,
+        private userID: number,
+        private carMileage: number,
+        private numberOfLiters: number,
+        private costBrutto: number | null,
+        private isFuelCardUsed: boolean,
+        ) {}
+
+    async addOneRefueling() {
+        await RefuelingModel.create({
+          id: this.id,
+          carID: this.carID,
+          userID: this.userID,
+          carMileage: this.carMileage,
+          numberOfLiters: this.numberOfLiters,
+          costBrutto: this.costBrutto,
+          isFuelCardUsed: this.isFuelCardUsed,
+        })
+    }
+
+    static async fetchAll() {
+        return await RefuelingModel.findAll()
+    }
+
+    static async fetchOne(id: number) {
+        return await RefuelingModel.findOne({ where: { id: id } })
+    }
+}
+
+
+
+export default Refueling;
