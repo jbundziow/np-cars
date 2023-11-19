419a1dfd8a0fc6a2c6c5e7dec109dbc8f1889a7a
jbundziow <jakub.bundziow@gmail.com>
2023-11-19 07:42:14 +0100

    added Place controller

---
+++ backend/src/controllers/placesController.ts
@@ -0,0 +1,11 @@
+
+import { NextFunction, Request, Response, response } from 'express'
+
+import Place from '../models/Place';
+
+
+
+
+export const fetchAllPlaces = async (req: Request, res: Response, next: NextFunction) => {
+
+}
