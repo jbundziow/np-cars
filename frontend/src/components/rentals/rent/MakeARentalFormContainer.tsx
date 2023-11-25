d5ecf37aff94b5bac97f2014d32c1686bce8093c
jbundziow <jakub.bundziow@gmail.com>
2023-11-25 20:17:05 +0100

    updated form

--- frontend/src/components/rentals/rent/MakeARentalFormContainer.tsx
+++ frontend/src/components/rentals/rent/MakeARentalFormContainer.tsx
@@ -34,15 +34,9 @@ const MakeARentalFormContainer = (props: MakeARentalFormContainerProps) => {
   const [warnings, setWarnings] = useState<warnings[]>([{en: 'Reason unknown. Unable to load error codes from server.', pl: 'Powód nieznany. Nie udało się wczytać kodów błędów z serwera.'}])
   const [pageState, setPageState] = useState<PageStatus>(PageStatus.FillingTheForm)
 
-  const [travelDestination, setTravelDestination] = useState<string>('');
-  const [value, setValue] = useState<DateValueType>({
-    startDate: formatDate(new Date()),
-    endDate: formatDate(new Date())
-  });
-
-  const handleValueChange = (newValue: DateValueType) => {
-    setValue(newValue);
-  };
+  const [carMileageBefore, setCarMileageBefore] = useState<number>();
+  const[showTravelDestinationInput, setShowTravelDestinationInput] = useState<boolean>(false);
+  
 
   
 
@@ -58,7 +52,7 @@ const MakeARentalFormContainer = (props: MakeARentalFormContainerProps) => {
           'Content-Type': 'application/json; charset=utf-8',
         },
         // TODO: ADD CORRECT USER!!!!!
-        body: JSON.stringify({carID: props.data.id, userID: 12, lastEditedByModeratorOfID: null, dateFrom: value?.startDate, dateTo: value?.endDate, travelDestination}),
+        // body: JSON.stringify({carID: props.data.id, userID: 12, lastEditedByModeratorOfID: null, dateFrom: value?.startDate, dateTo: value?.endDate, travelDestination}),
       });
 
       if (response.ok) {
@@ -100,46 +94,39 @@ const MakeARentalFormContainer = (props: MakeARentalFormContainerProps) => {
                     <form onSubmit={submitHandler}>
                       <div className='mb-5'>
                         <label className="mb-3 block text-black dark:text-white">
-                          Wybierz zakres dat, w którym chcesz dokonać rezerwacji:
-                        </label>
-                        {/* DOCS: https://react-tailwindcss-datepicker.vercel.app/install */}
-                        <Datepicker
-                        i18n={"pl"}
-                        separator={"do"}
-                        minDate={new Date()}
-                        displayFormat={"DD/MM/YYYY"}
-                        startWeekOn="mon"
-                        value={value}
-                        onChange={handleValueChange}
-                        inputClassName="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary" 
-
-                        showShortcuts={false}
-                        showFooter={false}
-                        configs={{footer: {
-                          cancel: "Anuluj", 
-                          apply: "Potwierdź" 
-                          }
-                        }}
-                        />
-                      </div>
-
-                      <div className='mb-5'>
-                        <label className="mb-3 block text-black dark:text-white">
-                          Wpisz cel podróży:
+                          Przebieg początkowy:
                         </label>
                         <input
                           required
-                          type="text"
-                          placeholder={`np. "Marba Racula"`}
+                          type="number"
+                          placeholder={`Wpisz przebieg samochodu przed rozpoczęciem podróży`}
                           className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                          value={travelDestination}
-                          onChange={e => setTravelDestination(e.target.value)}
+                          value={carMileageBefore}
+                          onChange={e => setCarMileageBefore(Number(e.target.value))}
                         />
+                      </div>
+
+                      <div className="col-span-1 flex flex-col sm:flex-row mb-10 sm:mb-5">
+                        <label className="block text-black dark:text-white col-span-2 sm:self-center mb-3 sm:mb-0">
+                          Czy chcesz już teraz wpisać cel swojej podróży?
+                        </label>
+                        <div className="sm:ml-3 relative z-20 bg-white dark:bg-form-input">
+                          <span className="absolute top-1/2 left-4 z-30 -translate-y-1/2">
+                          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><path fill="#3C50E0" d="M512 80c8.8 0 16 7.2 16 16v32H48V96c0-8.8 7.2-16 16-16H512zm16 144V416c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V224H528zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm56 304c-13.3 0-24 10.7-24 24s10.7 24 24 24h48c13.3 0 24-10.7 24-24s-10.7-24-24-24H120zm128 0c-13.3 0-24 10.7-24 24s10.7 24 24 24H360c13.3 0 24-10.7 24-24s-10.7-24-24-24H248z"/></svg>
+                          </span>
+                          <select className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-12 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input"
+                          value={showTravelDestinationInput}
+                          onChange={setShowTravelDestinationInput((e: HTMLInputElement) => (e.target.value)}
+                          >
+                            <option value={false}>Nie</option>
+                            <option value={true}>Tak</option>
+                          </select>
+                        </div>
                       </div>
 
                       <div className='flex justify-center mb-2'>
                       <button className="flex w-90 justify-center rounded bg-primary p-3 font-medium text-gray hover:opacity-90" type='submit'>
-                          Dokonaj rezerwacji
+                          Wypożycz samochód
                         </button>
                       </div>
                     </form>
