5f5e5922539755c0a3dba4ab10fd3b626091c707
jbundziow <jakub.bundziow@gmail.com>
2024-03-19 17:00:02 +0100

    added PieChart

---
+++ frontend/src/components/general/charts/PieChart.tsx
@@ -0,0 +1,131 @@
+import { ApexOptions } from 'apexcharts';
+import { useState } from 'react';
+import ReactApexChart from 'react-apexcharts';
+
+type PieChartProps = {
+  title: string,
+  data: {name: string, value: number}[]
+}
+
+interface PieChartState {
+  series: number[];
+}
+
+
+
+const PieChart = (props: PieChartProps) => {
+
+
+  
+  const [state, setState] = useState<PieChartState>({
+    series:  props.data.map((item) => item.value),
+  });
+
+
+
+
+  const options: ApexOptions = {
+    chart: {
+      type: 'donut',
+    },
+    colors: ['#10B981', '#375E83', '#259AE6', '#FFA70B'],
+    labels: props.data.map((item) => item.name),
+    legend: {
+      show: true,
+      position: 'bottom',
+    },
+  
+    plotOptions: {
+      pie: {
+        donut: {
+          size: '65%',
+          background: 'transparent',
+        },
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    responsive: [
+      {
+        breakpoint: 2600,
+        options: {
+          chart: {
+            width: 380,
+          },
+        },
+      },
+      {
+        breakpoint: 640,
+        options: {
+          chart: {
+            width: 200,
+          },
+        },
+      },
+    ],
+  };
+
+
+
+
+
+
+  const totalValues = props.data.map((item) => item.value).reduce((a, b) => a + b, 0);
+
+
+
+
+
+  let bgColors: string[] = [];
+  if(options.colors) {
+  bgColors = options.colors.map((color) => `bg-[${color}]`);
+  }
+
+
+
+
+
+  return (
+    <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-5">
+      <div className="mb-3 justify-between gap-4 sm:flex">
+        <div>
+          <h5 className="text-xl font-semibold text-black dark:text-white">
+            {props.title}
+          </h5>
+        </div>
+        <div>
+        </div>
+      </div>
+
+      <div className="mb-2">
+        <div id="PieChart" className="mx-auto flex justify-center">
+          <ReactApexChart
+            options={options}
+            series={state.series}
+            type="donut"
+          />
+        </div>
+      </div>
+
+      <div className="-mx-8 flex flex-wrap items-center justify-center gap-y-3 mt-10">
+        {props.data.map((item, index) => {
+          const color = bgColors[index];
+
+          return (
+          <div className="w-full px-8 sm:w-1/2">
+          <div className="flex w-full items-center">
+            <span className={`mr-2 block h-3 w-full max-w-3 rounded-full ${color}`}></span>
+            <p className="flex w-full justify-between text-xs font-medium text-black dark:text-white">
+              <span>&nbsp;{item.name}&nbsp;</span>
+              <span> {Number(item.value*100/totalValues).toFixed(2)}% </span>
+            </p>
+          </div>
+        </div>
+        )})}
+      </div>
+    </div>
+  );
+};
+
+export default PieChart;
--- frontend/src/components/users/UserPage.tsx
+++ frontend/src/components/users/UserPage.tsx
@@ -7,6 +7,10 @@ import { db_User } from '../../types/db_types';
 import useAuth from '../../hooks/useAuth';
 import { stats_UserDistanceInYear, stats_UserDistanceInYear_oneMonthSchema } from '../../types/stats';
 import { Link } from 'react-router-dom';
+import CardStat from '../general/CardStat';
+import ChartThree from '../ChartThree';
+import ChartOne from '../ChartOne';
+import PieChart from '../general/charts/PieChart';
 
 
 
@@ -116,6 +120,145 @@ const UserPage = (props: UserPageProps) => {
           {/* key={props.statsData.key} */}
           <BarChart title={'Przejechane kilometry'} data={props.statsData.distance.map((obj: stats_UserDistanceInYear_oneMonthSchema) => obj.total_distance)} categories={['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień']} filterBy={'year'} filterValue={props.filterValue} setFilterValue={(value: number) => {props.setFilterValue(value)}}/>
           </div>
+
+
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+            <CardStat
+            svg={
+            <svg
+              className="fill-primary dark:fill-white"
+              width="22"
+              height="16"
+              viewBox="0 0 22 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11 15.1156C4.19376 15.1156 0.825012 8.61876 0.687512 8.34376C0.584387 8.13751 0.584387 7.86251 0.687512 7.65626C0.825012 7.38126 4.19376 0.918762 11 0.918762C17.8063 0.918762 21.175 7.38126 21.3125 7.65626C21.4156 7.86251 21.4156 8.13751 21.3125 8.34376C21.175 8.61876 17.8063 15.1156 11 15.1156ZM2.26876 8.00001C3.02501 9.27189 5.98126 13.5688 11 13.5688C16.0188 13.5688 18.975 9.27189 19.7313 8.00001C18.975 6.72814 16.0188 2.43126 11 2.43126C5.98126 2.43126 3.02501 6.72814 2.26876 8.00001Z"
+                fill=""
+              />
+              <path
+                d="M11 10.9219C9.38438 10.9219 8.07812 9.61562 8.07812 8C8.07812 6.38438 9.38438 5.07812 11 5.07812C12.6156 5.07812 13.9219 6.38438 13.9219 8C13.9219 9.61562 12.6156 10.9219 11 10.9219ZM11 6.625C10.2437 6.625 9.625 7.24375 9.625 8C9.625 8.75625 10.2437 9.375 11 9.375C11.7563 9.375 12.375 8.75625 12.375 8C12.375 7.24375 11.7563 6.625 11 6.625Z"
+                fill=""
+              />
+            </svg>
+            }
+            value={3434+' km'}
+            title="Przejechany dystans w roku 2024"
+            showProgress={true}
+            progressValue={242+' km'}
+            isProgressPositive={true}
+
+            />
+
+            <CardStat
+            svg={
+            <svg
+              className="fill-primary dark:fill-white"
+              width="22"
+              height="16"
+              viewBox="0 0 22 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11 15.1156C4.19376 15.1156 0.825012 8.61876 0.687512 8.34376C0.584387 8.13751 0.584387 7.86251 0.687512 7.65626C0.825012 7.38126 4.19376 0.918762 11 0.918762C17.8063 0.918762 21.175 7.38126 21.3125 7.65626C21.4156 7.86251 21.4156 8.13751 21.3125 8.34376C21.175 8.61876 17.8063 15.1156 11 15.1156ZM2.26876 8.00001C3.02501 9.27189 5.98126 13.5688 11 13.5688C16.0188 13.5688 18.975 9.27189 19.7313 8.00001C18.975 6.72814 16.0188 2.43126 11 2.43126C5.98126 2.43126 3.02501 6.72814 2.26876 8.00001Z"
+                fill=""
+              />
+              <path
+                d="M11 10.9219C9.38438 10.9219 8.07812 9.61562 8.07812 8C8.07812 6.38438 9.38438 5.07812 11 5.07812C12.6156 5.07812 13.9219 6.38438 13.9219 8C13.9219 9.61562 12.6156 10.9219 11 10.9219ZM11 6.625C10.2437 6.625 9.625 7.24375 9.625 8C9.625 8.75625 10.2437 9.375 11 9.375C11.7563 9.375 12.375 8.75625 12.375 8C12.375 7.24375 11.7563 6.625 11 6.625Z"
+                fill=""
+              />
+            </svg>
+            }
+            value={122}
+            title="Podróży w roku 2024"
+            showProgress={true}
+            progressValue={21.23+'%'}
+            isProgressPositive={false}
+
+            />
+
+            <CardStat
+            svg={
+            <svg
+              className="fill-primary dark:fill-white"
+              width="22"
+              height="16"
+              viewBox="0 0 22 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11 15.1156C4.19376 15.1156 0.825012 8.61876 0.687512 8.34376C0.584387 8.13751 0.584387 7.86251 0.687512 7.65626C0.825012 7.38126 4.19376 0.918762 11 0.918762C17.8063 0.918762 21.175 7.38126 21.3125 7.65626C21.4156 7.86251 21.4156 8.13751 21.3125 8.34376C21.175 8.61876 17.8063 15.1156 11 15.1156ZM2.26876 8.00001C3.02501 9.27189 5.98126 13.5688 11 13.5688C16.0188 13.5688 18.975 9.27189 19.7313 8.00001C18.975 6.72814 16.0188 2.43126 11 2.43126C5.98126 2.43126 3.02501 6.72814 2.26876 8.00001Z"
+                fill=""
+              />
+              <path
+                d="M11 10.9219C9.38438 10.9219 8.07812 9.61562 8.07812 8C8.07812 6.38438 9.38438 5.07812 11 5.07812C12.6156 5.07812 13.9219 6.38438 13.9219 8C13.9219 9.61562 12.6156 10.9219 11 10.9219ZM11 6.625C10.2437 6.625 9.625 7.24375 9.625 8C9.625 8.75625 10.2437 9.375 11 9.375C11.7563 9.375 12.375 8.75625 12.375 8C12.375 7.24375 11.7563 6.625 11 6.625Z"
+                fill=""
+              />
+            </svg>
+            }
+            value={2284.21}
+            title="Zatankowane litry paliwa w roku 2024"
+            showProgress={true}
+            progressValue={63.21+'%'}
+            isProgressPositive={true}
+
+            />
+
+
+          <CardStat
+            svg={
+            <svg
+              className="fill-primary dark:fill-white"
+              width="22"
+              height="16"
+              viewBox="0 0 22 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11 15.1156C4.19376 15.1156 0.825012 8.61876 0.687512 8.34376C0.584387 8.13751 0.584387 7.86251 0.687512 7.65626C0.825012 7.38126 4.19376 0.918762 11 0.918762C17.8063 0.918762 21.175 7.38126 21.3125 7.65626C21.4156 7.86251 21.4156 8.13751 21.3125 8.34376C21.175 8.61876 17.8063 15.1156 11 15.1156ZM2.26876 8.00001C3.02501 9.27189 5.98126 13.5688 11 13.5688C16.0188 13.5688 18.975 9.27189 19.7313 8.00001C18.975 6.72814 16.0188 2.43126 11 2.43126C5.98126 2.43126 3.02501 6.72814 2.26876 8.00001Z"
+                fill=""
+              />
+              <path
+                d="M11 10.9219C9.38438 10.9219 8.07812 9.61562 8.07812 8C8.07812 6.38438 9.38438 5.07812 11 5.07812C12.6156 5.07812 13.9219 6.38438 13.9219 8C13.9219 9.61562 12.6156 10.9219 11 10.9219ZM11 6.625C10.2437 6.625 9.625 7.24375 9.625 8C9.625 8.75625 10.2437 9.375 11 9.375C11.7563 9.375 12.375 8.75625 12.375 8C12.375 7.24375 11.7563 6.625 11 6.625Z"
+                fill=""
+              />
+            </svg>
+            }
+            value={5}
+            title="Rezerwacji w roku 2024"
+            showProgress={true}
+            progressValue={2.42+'%'}
+            isProgressPositive={false}
+
+            />
+            
+
+          </div>
+
+
+          <div className="md:my-10 md:mx-2 grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
+            <PieChart
+            title={'Top 4 projekty w roku 2024 według przejechanego dystansu'}
+            data={
+              [
+                {name: '103/23/MCZ', value: 400},
+                {name: '52/23', value: 200},
+                {name: '92/25/FFSA', value: 300},
+                {name: '23/112/AD', value: 100},
+              ]
+            }
+            />
+            <div className='col-span-7'>
+
+            <ChartOne/>
+            </div>
+          </div>
+
+
 
           </div>
         </div>
