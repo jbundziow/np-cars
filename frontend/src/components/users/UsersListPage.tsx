72f796d96a7d87ba60fa9feb3c32f816243e871f
jbundziow <jakub.bundziow@gmail.com>
2024-12-24 00:50:49 +0100

    admin is not visible in user list

--- frontend/src/components/users/UsersListPage.tsx
+++ frontend/src/components/users/UsersListPage.tsx
@@ -21,7 +21,7 @@ const isUnconfirmedUserExist = props.users.some(user => user.role === 'unconfirm
       <> 
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        {props.users.filter(user => user.role !== 'banned' && user.role !== 'unconfirmed').map(user => <CardUser userData={user} primaryButton={{text: 'Szczegóły', link: `/uzytkownicy/${user.id}`}} secondaryButton={{text: 'Edytuj', link: `/uzytkownicy/ustawienia-konta/${user.id}`}} auth={props.auth}/>)}
+        {props.users.filter(user => user.role !== 'banned' && user.role !== 'unconfirmed' && user.id !== 9999).map(user => <CardUser userData={user} primaryButton={{text: 'Szczegóły', link: `/uzytkownicy/${user.id}`}} secondaryButton={{text: 'Edytuj', link: `/uzytkownicy/ustawienia-konta/${user.id}`}} auth={props.auth}/>)}
       </div>
 
 
